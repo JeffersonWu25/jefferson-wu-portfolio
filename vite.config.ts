0b2cf5b5db3c4ec0d4d4c7392202c71c966e4178
Jefferson Wu <jeffersonw888@gmail.com>
2025-08-27 19:28:18 -0400

    fixed deploy for vite

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/jefferson-wu-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
