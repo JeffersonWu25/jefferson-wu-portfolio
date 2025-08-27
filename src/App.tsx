59cca14626a4661fa84192e2ee1452e3d0ddea8c
Jefferson Wu <jeffersonw888@gmail.com>
2025-08-27 19:42:02 -0400

    fixed router pt3

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/jefferson-wu-portfolio">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
